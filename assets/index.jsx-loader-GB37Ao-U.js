f1cded5fedda7e8359df590a1ea258d84ad8d840
Steamauth Deploy Service <info@steamwebapi.com>
2024-04-26 01:17:29 +0000

    NEW BUILD

--- assets/index.jsx-loader-GB37Ao-U.js
+++ assets/index.jsx-loader-GB37Ao-U.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/index.jsx-cCKdT8sc.js")
+      chrome.runtime.getURL("assets/index.jsx-C6HC0Sp2.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
